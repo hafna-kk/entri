0511c60256097423744ed0580fadfeae3eb382ca
hafna mol k <hafnakk@gmail.com>
2025-12-12 09:53:01 +0530

    new traffic control system

--- AVR Programming/traffic-dc/traffic-dc/main.c
+++ AVR Programming/traffic-dc/traffic-dc/main.c
@@ -8,92 +8,123 @@
  * Author : hafna
  */ 
 
-
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-//ISR for external interrupt
-ISR(INT0_vect)
-{
-	stp();          // Force to RED
+volatile uint8_t interrupt_flag = 0;   // flag set by ISR
 
-	cw();           // CW for 10 seconds
-	delay(10000);
 
-	motor_stop();   // Stop 20 seconds
-	delay(20000);
-
-	acw();          // ACW for 10 seconds
-	delay(10000);
-
-	motor_stop();
-	go();           // Back to GREEN
-}
-void delay(unsigned int t){ //set delay function
+// delay function
+void delay_ms(unsigned int t){
 	while(t--){
+		   if (interrupt_flag == 1) return; 
 		_delay_ms(1);
 	}
 }
 
+
 // TRAFFIC LIGHT
-void stp(){           // RED ON
+void stp(void){
 	PORTB &= ~((1<<PB1)|(1<<PB2)|(1<<PB3));
-	PORTB |= (1<<PB3);
-}
-void ready(){         // ORANGE ON
-	PORTB &= ~((1<<PB1)|(1<<PB2)|(1<<PB3));
-	PORTB |= (1<<PB2);
-}
-void go(){            // GREEN ON
-	PORTB &= ~((1<<PB1)|(1<<PB2)|(1<<PB3));
-	PORTB |= (1<<PB1);
+	PORTB |= (1<<PB3);    // RED
 }
 
-// MOTOR FUNCTIONS
-void cw(){
+void ready(void){
+	PORTB &= ~((1<<PB1)|(1<<PB2)|(1<<PB3));
+	PORTB |= (1<<PB2);    // ORANGE
+}
+
+void go(void){
+	PORTB &= ~((1<<PB1)|(1<<PB2)|(1<<PB3));
+	PORTB |= (1<<PB1);    // GREEN
+}
+
+
+// MOTOR
+void cw(void){
 	PORTD |= (1<<PD0);
 	PORTD &= ~(1<<PD1);
 }
-void acw(){
+
+void acw(void){
 	PORTD |= (1<<PD1);
 	PORTD &= ~(1<<PD0);
 }
-void motor_stop(){
+
+void motor_stop(void){
 	PORTD &= ~(1<<PD0);
 	PORTD &= ~(1<<PD1);
 }
 
+//ISR for External Interrupt
+ISR(INT0_vect)
+{
+	interrupt_flag = 1;   // set a flag
+}
+
+
+
+
 int main(void)
 {
-	// motor pins output
-	DDRD |= (1<<DDD0) | (1<<DDD1)|(1<<DDD6); //PD6 is pwm pin
+	// motor pins
+	DDRD |= (1<<DDD0) | (1<<DDD1) | (1<<DDD6);
 
-	// traffic light pins output
+	// traffic light pins
 	DDRB |= (1<<DDB1) | (1<<DDB2) | (1<<DDB3);
 
-	// switch input
-	DDRD &= ~(1<<DDD2);   // PD2 (INT0) as input
-	PORTD |= (1<<PD2);    // enable pull-up
+	// switch
+	DDRD &= ~(1<<DDD2);
+	PORTD |= (1<<PD2);  // enable pull-up
 
-	PORTD |= (1<<PD6);   // ENABLE L293D output permanently
-	EIFR|=(1<<INTF0);// Clear flag
-	
-	EICRA|=((1<<ISC00)|(1<<ISC01)); //Configure INT0 for rising edge
-	
-	EIMSK|=(1<<INT0);//Enable External interrupt0
-	
-	sei(); //Enable global interrupt
+	PORTD |= (1<<PD6);  // enable L293D output
+
+	// interrupt configure
+	// INT0 low-level trigger
+	EICRA &= ~((1<<ISC00) | (1<<ISC01));  // ISC01 = 0, ISC00 = 0
+
+	EIFR |= (1<<INTF0);
+	EIMSK |= (1<<INT0); //configure external interrupt
+	sei();//enable global interrupt
+
 	while(1)
 	{
-		// normal sequence
-		go();      delay(5000);
-		ready();   delay(3000);
-		stp();     delay(5000);
-		ready();   delay(3000);
+		// If switch pressed   //ISR
+		if(interrupt_flag == 1)
+		{
+			
 
+			 interrupt_flag = 0;//clear the flag
+			stp();                // red
+			cw();                 // CW 10s
+			delay_ms(10000);
 
-		
+			motor_stop();         // stop 20s
+			delay_ms(20000);
+
+			acw();                // ACW 10s
+			delay_ms(10000);
+
+			motor_stop();
+			go();                 // back to green
+			continue;
+		}
+
+		// NORMAL TRAFFIC SYSTEM
+	go();      
+	delay_ms(5000);
+	if(interrupt_flag == 1) continue;
+
+	ready();  
+	 delay_ms(3000);
+	if(interrupt_flag == 1) continue;
+
+	stp();    
+	 delay_ms(5000);
+	if(interrupt_flag == 1) continue;
+
+	ready();   delay_ms(3000);
 	}
 }
